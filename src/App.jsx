8ab8cdf6e09a5558b865d330cc25774fbfeed576
Rahul Chhabra <chhabrarahul2707@gmail.com>
2024-11-06 12:37:56 +0530

    stage-03/useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client"
-import Pet from './Pet.jsx';
-
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
   return (
     <div>
-      <Pet name="Kittu" animal="Dog" breed="Havenese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Doink" animal="Cat" breed="Mix"></Pet>
+      <h1>Adopt Me</h1>
+      <SearchParams />
     </div>
   );
-}
+};
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App></App>);
-
-
-/**
- *  props : { name, animal, breed }, value = 10 passing
- * 
- * {  props : { name, animal, breed }, value = 10 } Recieving
- *  { props : { name, animal, breed }, value = 10 }
- * 
- * name,animal,value
- * 
- * { name,animal,value }
- * { name, animal, breed }
-*/
-
-
-// // App Component
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Kittu",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-//   ]);
-// };
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-
-// root.render(React.createElement(App));
-
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 
-
-const Pet = (animalData) => {
-  console.log(animalData)
+const Pet = ({ name, animal, breed }) => {
   return (
     <div>
-      <h1>{animalData.name}</h1>
-      <h2>{animalData.animal}</h2>
-      <h2>{animalData.breed}</h2>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
     </div>
-  )
-}
-// const Pet = (props) => {
-//   console.log(props);
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-// };
-// { name, animal, breed }
+  );
+};
 export default Pet;
